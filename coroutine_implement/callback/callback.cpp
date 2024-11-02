35b54f64eb38ed98923abebe5a7380d1de529ce2
asice <2492215094@qq.com>
2024-11-02 14:53:47 +0800

    fix:callback in cpp

--- coroutine_implement/callback/callback.cpp
+++ coroutine_implement/callback/callback.cpp
@@ -1,47 +1,53 @@
 #include <fstream>
 #include <functional>
 #include <iostream>
+#include <future>
 
 using CallBack = std::function<void(const std::string &)>;
 
-void async_read(const std::string &filename, CallBack cb) {
-  std::ifstream file(filename);
-  if (!file.is_open()) {
-    cb("error: unable to open");
-    return;
-  }
+void async_read(const std::string &filename, CallBack cb)
+{
+  std::async(std::launch::async, [filename, cb]()
+             {
+    std::ifstream file(filename);
+    if (!file.is_open()) {
+      cb("error: unable to open");
+      return;
+    }
 
-  std::string content((std::istreambuf_iterator<char>(file)),
-                      std::istreambuf_iterator<char>());
-  file.close();
+    std::string content((std::istreambuf_iterator<char>(file)),
+                        std::istreambuf_iterator<char>());
+    file.close();
 
-  cb(content);
+    cb(content); });
 }
 
 void async_write(const std::string &filename, const std::string &content,
-                 CallBack cb) {
-  std::ofstream file(filename);
-  if (!file.is_open()) {
-    cb("Error: unable to open file");
-    return;
-  }
+                 CallBack cb)
+{
+  std::async(std::launch::async, [filename, content, cb]()
+             {
+    std::ofstream file(filename);
+    if (!file.is_open()) {
+      cb("Error: unable to open file");
+      return;
+    }
 
-  file << content;
-  file.close();
-
-  cb("Write successful");
+    file << content;
+    file.close();
+    cb("Write successful"); });
 }
 
-int main() {
-  // 异步读取文件
-  async_read("example.txt", [](const std::string &content) {
-    std::cout << "Read content: " << content << std::endl;
-  });
+int main()
+{
+  async_read("callback.cpp", [](const std::string &content)
+             {
+    std::cout << "Read successful\n";
+    std::cout << content << std::endl; });
 
-  // 异步写入文件
-  async_write("example.txt", "Hello, world!", [](const std::string &result) {
-    std::cout << "Write result: " << result << std::endl;
-  });
+  async_write("callback.txt", "Hello, callback", [](const std::string &msg)
+              { std::cout << msg << std::endl; });
 
+  std::this_thread::sleep_for(std::chrono::seconds(1));
   return 0;
 }
