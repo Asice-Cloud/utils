50a5989ec8035024196bb4bd77df33bbdfd5f5d7
asice <2492215094@qq.com>
2025-03-13 13:11:11 +0800

    concept

--- a_useflu_sample/template_usage/concept1.cpp
+++ a_useflu_sample/template_usage/concept1.cpp
@@ -1,8 +1,14 @@
-#include <variant>
+#include <iostream>
+#include <type_traits>
 
-template <typename... T>
-concept is_int = requires{
-  std::is_same_v<std::variant<T...>,std::variant<int>>;
+template <typename T>
+concept is_int = requires(T t) { std::is_same_v<std::decay_t<T>, int>; };
+
+template <typename T>
+void print_int(T t)
+  requires is_int<T>
+{
+  std::cout << t << " " << "is int" << '\n';
 }
 
-int main() {}
+int main() { print_int(11); }
