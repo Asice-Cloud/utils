d42b0e92eda5ba299da699bda5b09e5d04934e76
asice <2492215094@qq.com>
2025-04-28 18:14:21 +0800

    coroutine in nw

---
+++ nw/src/core/awaiter.h
@@ -0,0 +1,8 @@
+//
+// Created by asice-cloud on 4/28/25.
+//
+
+#ifndef AWAITER_H
+#define AWAITER_H
+
+#endif //AWAITER_H
---
+++ nw/src/core/task.h
@@ -0,0 +1,8 @@
+//
+// Created by asice-cloud on 4/28/25.
+//
+
+#ifndef TASK_H
+#define TASK_H
+
+#endif //TASK_H
