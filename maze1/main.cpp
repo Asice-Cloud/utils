f0166d838c1651319ede5b9024ccc4d4080b3361
Asice Cloud <2492215094@qq.com>
2025-11-01 21:57:21 +0800

    remove dist

--- maze1/main.cpp
+++ maze1/main.cpp
@@ -1,18 +1,18 @@
+#include "labyrinthwidget.h"
 #include <QApplication>
 #include <QDebug>
-#include <QPointer>
 #include <QMessageBox>
-#include "labyrinthwidget.h"
+#include <QPointer>
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
     QApplication a(argc, argv);
 
     // Ask the player if they want to enable surprise mode
     bool surpriseMode = false;
     QMessageBox msgBox;
     msgBox.setWindowTitle("Game Mode");
-    msgBox.setText("Do you want to enable Surprise Mode? (Portals will appear if enabled)");
+    msgBox.setText("Do you want to enable Surprise Mode? (Portals will appear "
+                   "if enabled)");
     msgBox.setStandardButtons(QMessageBox::Yes | QMessageBox::No);
     msgBox.setDefaultButton(QMessageBox::No);
     int ret = msgBox.exec();
@@ -27,28 +27,33 @@ int main(int argc, char *argv[])
 
     QPointer<LabyrinthWidget> portalWindow = nullptr;
 
-    QObject::connect(mainWindow, &LabyrinthWidget::requestOpenPortal, [&]()
-                     {
-        if (!mainWindow->isSurpriseMode()) return;
-        if (portalWindow) return;
+    QObject::connect(mainWindow, &LabyrinthWidget::requestOpenPortal, [&]() {
+        if (!mainWindow->isSurpriseMode())
+            return;
+        if (portalWindow)
+            return;
         mainWindow->setEnabled(false);
         portalWindow = new LabyrinthWidget();
         portalWindow->setSurpriseMode(true);
         portalWindow->setIsPortalWorld(true);
         portalWindow->setWindowTitle("Limbo 嘻嘻,我一定要活下去");
         portalWindow->resize(600, 400);
-        portalWindow->setReturnPosition(mainWindow->getPortalBRow(), mainWindow->getPortalBCol());
+        portalWindow->setReturnPosition(mainWindow->getPortalBRow(),
+                                        mainWindow->getPortalBCol());
         portalWindow->show();
-        QObject::connect(portalWindow, &LabyrinthWidget::requestReturnFromPortal, mainWindow, [&](int row, int col) {
-            mainWindow->setPlayerRow(row);
-            mainWindow->setPlayerCol(col);
-            mainWindow->removePortal(); // Remove portal after returning
-            mainWindow->update();
-            mainWindow->setEnabled(true);
-            portalWindow->close();
-            portalWindow->deleteLater();
-            portalWindow = nullptr;
-        }); });
+        QObject::connect(
+            portalWindow, &LabyrinthWidget::requestReturnFromPortal, mainWindow,
+            [&](int row, int col) {
+                mainWindow->setPlayerRow(row);
+                mainWindow->setPlayerCol(col);
+                mainWindow->removePortal(); // Remove portal after returning
+                mainWindow->update();
+                mainWindow->setEnabled(true);
+                portalWindow->close();
+                portalWindow->deleteLater();
+                portalWindow = nullptr;
+            });
+    });
 
     return a.exec();
 }
