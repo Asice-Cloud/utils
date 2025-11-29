99b264592bb48704747ad5abd1fd88820e56722e
Asice Cloud <2492215094@qq.com>
2025-11-30 00:53:22 +0800

    ctl

--- ctl/test.c
+++ ctl/test.c
@@ -4,16 +4,15 @@
 
 #include <stdio.h>
 
-int main()
-{
+int main() {
     vec_int vi = vec_int_init();
     vec_int_push_back(&vi, 42);
     vec_int_push_back(&vi, 84);
     vec_int_push_back(&vi, 126);
 
-    for (size_t i = 0; i < vi.size; i++)
-    {
+    for (size_t i = 0; i < vi.size; i++) {
         printf("%d ", *vec_int_at(&vi, i));
     }
+
     printf("\n");
 }
