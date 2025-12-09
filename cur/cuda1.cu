f08441ab3ce2e03b93bdf30c87a1c84aac598037
Asice Cloud <2492215094@qq.com>
2025-12-10 01:33:09 +0800

    matrix add

--- cur/cuda1.cu
+++ cur/cuda1.cu
@@ -4,9 +4,62 @@
 
 __global__ void helloFromGPU() { printf("Hello World from GPU!\n"); }
 
+__global__ void MatrixAdd(const float *A, const float *B, float *C, int N) {
+    int row = blockIdx.y * blockDim.y + threadIdx.y;
+    int col = blockIdx.x * blockDim.x + threadIdx.x;
+
+    if (row < N && col < N) {
+        int idx = row * N + col;
+        C[idx] = A[idx] + B[idx];
+    }
+}
+
 int main() {
     // Launch kernel with 1 block and 1 thread
     helloFromGPU<<<1, 1>>>();
+
+    const int N = 10;
+    size_t size = N * N * sizeof(float);
+    float *h_A = (float *)malloc(size);
+    float *h_B = (float *)malloc(size);
+    float *h_C = (float *)malloc(size);
+
+    // Initialize matrices A and B
+    for (int i = 0; i < N * N; i++) {
+        h_A[i] = static_cast<float>(i);
+        h_B[i] = static_cast<float>(i * 2);
+    }
+
+    float *d_A, *d_B, *d_C;
+    cudaMalloc((void **)&d_A, size);
+    cudaMalloc((void **)&d_B, size);
+    cudaMalloc((void **)&d_C, size);
+
+    cudaMemcpy(d_A, h_A, size, cudaMemcpyHostToDevice);
+    cudaMemcpy(d_B, h_B, size, cudaMemcpyHostToDevice);
+    dim3 threadsPerBlock(16, 16);
+    dim3 blocksPerGrid((N + threadsPerBlock.x - 1) / threadsPerBlock.x,
+                       (N + threadsPerBlock.y - 1) / threadsPerBlock.y);
+    MatrixAdd<<<blocksPerGrid, threadsPerBlock>>>(d_A, d_B, d_C, N);
+    cudaMemcpy(h_C, d_C, size, cudaMemcpyDeviceToHost);
+    // Print result matrix C
+
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            printf("%0.1f ", h_C[i * N + j]);
+        }
+        printf("\n");
+    }
+
+    // Free device memory
+    cudaFree(d_A);
+    cudaFree(d_B);
+    cudaFree(d_C);
+
+    // Free host memory
+    free(h_A);
+    free(h_B);
+    free(h_C);
 
     // Wait for GPU to finish before accessing on host
     cudaDeviceSynchronize();
